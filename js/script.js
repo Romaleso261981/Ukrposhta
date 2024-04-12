1f6144c8ceb7ba55e3243b99719ec37cef22c528
Roman lesyo <98737355+Romaleso261981@users.noreply.github.com>
2024-04-12 12:45:42 +0300

    remove some code

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-const swiper = new Swiper('.materials__slider', {
-   loop: true,
-   autoplay: {
-      delay: 5000,
-   },
-   pagination: {
-      el: '.swiper-pagination',
-		clickable: true,
-   },
-});
+// const swiper = new Swiper('.materials__slider', {
+//    loop: true,
+//    autoplay: {
+//       delay: 5000,
+//    },
+//    pagination: {
+//       el: '.swiper-pagination',
+// 		clickable: true,
+//    },
+// });
 
-window.addEventListener('scroll', () => {
-	const header = document.querySelector('.header');
-	header.classList.toggle('sticky', window.scrollY > 0);
-});
+// window.addEventListener('scroll', () => {
+// 	const header = document.querySelector('.header');
+// 	header.classList.toggle('sticky', window.scrollY > 0);
+// });
